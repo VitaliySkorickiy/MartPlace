8dbc2ff68d21d6b858524496b4d8f4d1fdcf81dc
zarias2020 <zarias@yandex.ru>
2020-04-11 21:40:45 +0300

    added relrase products

--- app/js/main.js
+++ app/js/main.js
@@ -4,12 +4,19 @@ $(function(){
     $('.weekly__slid').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        arrows: false,
+        arrows: true,
+        prevArrow:'<button class="slick-arrow slick-prev"><img src="../app/img/icons/chevron-left.png" alt=""></button>',
+        nextArrow:'<button class="slick-arrow slick-next"><img src="../app/img/icons/chevron-right.png" alt=""></button>',
 
     });
 
+    $('.rate-star').rateYo({
+        rating: 4.4,
+        starWidth: "15px",
+        readOnly: true
+    });
 
-    
+    var mixer = mixitup('.release__product-wrapper');
 
 
 
